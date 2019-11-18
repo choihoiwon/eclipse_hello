cfbb04786e7b8250343feb04f3bc46f41ae984ec
choihoiwon <chlghldnjs12@gmail.com>
2019-11-18 18:11:11 +0900

    ec

--- src/study/chw20153039.java
+++ src/study/chw20153039.java
@@ -5,6 +5,7 @@ public class chw20153039 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System. out.println ("20153039 최회원 원격저장소에 올리기 ");
+		System. out.println ("이클립스에서 수정하기  ");
 	}
 
 }
